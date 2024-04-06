48dd67b30bfa2671baa8ac1cb866227f0d595a30
gabrieutm <gtmachado08@gmail.com>
2024-04-06 12:30:33 -0300

    STYLES ON SPREADSHEET
    Styles could only be added on xlsx files, therefore changed the writer to create a xlsx file

--- php/src/WebScrapping/Main.php
+++ php/src/WebScrapping/Main.php
@@ -5,6 +5,9 @@ namespace Chuva\Php\WebScrapping;
 require_once 'vendor/autoload.php';
 
 use OpenSpout\Writer\Common\Creator\WriterEntityFactory;
+use OpenSpout\Writer\Common\Creator\Style\StyleBuilder;
+use OpenSpout\Common\Entity\Style\CellAlignment;
+use OpenSpout\Common\Entity\Style\Color;
 
 /**
  * Runner for the Webscrapping exercice.
@@ -25,14 +28,32 @@ class Main {
     $columns_name = $result['columns'];
     $data = $result['data'];
 
-    $filename = 'teste.csv';
-    $writer = WriterEntityFactory::createCSVWriter();
+    $filename = 'teste.xlsx';
+    $writer = WriterEntityFactory::createXLSXWriter();
     $writer->openToFile($filename);
 
-    $writer->addRow(WriterEntityFactory::createRowFromArray($columns_name));
+    $style_columns = (new StyleBuilder())
+                    ->setFontBold()
+                    ->setFontName('Arial')
+                    ->setFontSize(14)
+                    ->setFontColor(Color::WHITE)
+                    ->setCellAlignment(CellAlignment::CENTER)
+                    ->setBackgroundColor(Color::BLUE)
+                    ->build();
+    
+    $style_items = (new StyleBuilder())
+                    ->setFontName('Arial')
+                    ->setFontSize(10)
+                    ->setFontColor(Color::BLACK)
+                    ->setShouldWrapText(true)
+                    ->setCellAlignment(CellAlignment::LEFT)
+                    ->setBackgroundColor(Color::WHITE)
+                    ->build();
+
+    $writer->addRow(WriterEntityFactory::createRowFromArray($columns_name, $style_columns));
 
     foreach ($data as $item) {
-      $writer->addRow(WriterEntityFactory::createRowFromArray($item));
+      $writer->addRow(WriterEntityFactory::createRowFromArray($item, $style_items));
     }
 
     $writer->close();
